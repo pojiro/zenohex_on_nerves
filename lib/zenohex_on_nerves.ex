547085b4f0ba1d5832ffdc433a15a3ef23a2cd07
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-22 18:26:05 +0900

    Initial commit

---
+++ lib/zenohex_on_nerves.ex
@@ -0,0 +1,18 @@
+defmodule ZenohexOnNerves do
+  @moduledoc """
+  Documentation for ZenohexOnNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZenohexOnNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zenohex_on_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule ZenohexOnNervesTest do
+  use ExUnit.Case
+  doctest ZenohexOnNerves
+
+  test "greets the world" do
+    assert ZenohexOnNerves.hello() == :world
+  end
+end
